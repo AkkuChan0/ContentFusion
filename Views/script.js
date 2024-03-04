32aef7d472e95efdb4ed6a7e8b54c912b700e1b5
Ivan Luchegov <108276321+AkkuChan0@users.noreply.github.com>
2024-03-04 22:56:13 +0700

    knockout + кнопочки потыкать + цветовая схема новая

--- Views/script.js
+++ Views/script.js
@@ -1,19 +1,17 @@
+
+
 function AppViewModel() {
 
 }
 
 var self = this;
-self.categories = {
-    new: 'new',
-    popular: 'popular',
-    other: []
-}
 
 self.project = {
-    nowCategory: ko.observable('new')
+    nowCategory: ko.observable()
 }
 
 // Activates knockout.js
 $(document).ready(function() {
     ko.applyBindings(new AppViewModel());
+    self.project.nowCategory(self.categoriesTemp().categories[0]);
 })
