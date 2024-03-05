6eea577bc8fdb3577917c5d974be42cfc960291b
Ivan Luchegov <108276321+AkkuChan0@users.noreply.github.com>
2024-03-05 21:23:27 +0700

    MVVM и перенос пробных данных в json

--- Views/script.js
+++ Views/script.js
@@ -4,6 +4,29 @@ function AppViewModel() {
 
 }
 
+self.categoriesTemp = ko.observable();
+self.articlesTitle = ko.observable();
+self.articlesFull = ko.observable();
+
+$.ajax({
+    url: 'data/categories.json',
+    dataType: 'json',
+    success: function(data) {
+        self.categoriesTemp = ko.mapping.fromJS(data.categories)
+        self.project.nowCategory(self.categoriesTemp()[0]);
+    }
+});
+
+$.ajax({
+    url: 'data/articlesTitle.json',
+    dataType: 'json',
+    success: function(data) {
+        self.articlesTitle = ko.mapping.fromJS(data.articlesTitle)
+        self.articlesTitle(self.articlesTitle().sort((a, b) => a.date() < b.date()));
+        self.popular = self.articlesTitle().sort((a, b) => a.rating() > b.rating());
+    }
+});
+
 var self = this;
 
 self.project = {
@@ -13,6 +36,4 @@ self.project = {
 // Activates knockout.js
 $(document).ready(function() {
     ko.applyBindings(new AppViewModel());
-    self.project.nowCategory(self.categoriesTemp().categories[0]);
-    self.articlesTitle(self.articlesTitle().sort((a, b) => a.date < b.date))
 })
