3f566af53f721cef25dc68e478dd70fd5f8c6008
Ivan Luchegov <108276321+AkkuChan0@users.noreply.github.com>
2024-03-08 16:10:19 +0700

    minor

--- Views/script.js
+++ Views/script.js
@@ -4,22 +4,20 @@ function AppViewModel() {
 
 }
 
+var self = this;
+ 
+self.project = {
+    nowCategory: ko.observable()
+}
+
 self.categoriesTemp = ko.observable();
 self.articlesTitle = ko.observable();
 self.articlesFull = ko.observable();
 
 $.ajax({
-    url: 'data/categories.json',
-    dataType: 'json',
-    success: function(data) {
-        self.categoriesTemp = ko.mapping.fromJS(data.categories)
-        self.project.nowCategory(self.categoriesTemp()[0]);
-    }
-});
-
-$.ajax({
     url: 'data/articlesTitle.json',
     dataType: 'json',
+    async: false,
     success: function(data) {
         self.articlesTitle = ko.mapping.fromJS(data.articlesTitle)
         self.articlesTitle(self.articlesTitle().sort((a, b) => a.date() < b.date()));
@@ -27,11 +25,15 @@ $.ajax({
     }
 });
 
-var self = this;
-
-self.project = {
-    nowCategory: ko.observable()
-}
+$.ajax({
+    url: 'data/categories.json',
+    dataType: 'json',
+    async: false,
+    success: function(data) {
+        self.categoriesTemp = ko.mapping.fromJS(data.categories);
+        self.project.nowCategory(self.categoriesTemp()[0].name());
+    }
+});
 
 // Activates knockout.js
 $(document).ready(function() {
